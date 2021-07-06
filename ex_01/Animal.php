a1b7cb9b2aabea718dbeeb7660948f9437c5d64e
root <root@LAPTOP-D55E9K9Q.localdomain>
2021-07-06 11:30:55 +0200

    addeed ex_01 file

---
+++ ex_01/Animal.php
@@ -0,0 +1,11 @@
+<?php
+class Animal {
+    const MAMMAL = 0;
+    const FISH = 1;
+    const BIRD = 2;
+    public function __construct() {
+        $this->name = $name
+        $this->
+    }
+}
+?>
